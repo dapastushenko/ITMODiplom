01db6acfacf4c691dfd3e22f94382138ed5039b3
dapastushenko <32649673+dapastushenko@users.noreply.github.com>
2017-11-27 19:31:05 +0300

    проверка места локально, начат сервер

---
+++ src/com/itmo/diplom/DiskSpaceDetails.java
@@ -0,0 +1,35 @@
+package com.itmo.diplom;
+import java.io.File;
+
+public class DiskSpaceDetails {
+
+
+    public static void main(String[] args) {
+        File[] roots = File.listRoots();
+        for (File file: roots) {
+
+            long totalSpace = file.getTotalSpace();
+            long freeSpace = file.getFreeSpace();
+            long usableSpace = file.getUsableSpace();
+
+            System.out.println("========== Раздел \"" + file.getAbsolutePath()
+                    + "\" (байты) ==========");
+            System.out.println("Totlal space (всего места): "
+                    + totalSpace);
+            System.out.println("Free space (свободное место): "
+                    + freeSpace);
+            System.out.println("Usable space (доступно для использования): "
+                    + usableSpace);
+            System.out.println("========== Раздел \"" + file.getAbsolutePath()
+                    + "\" (мегабайты) ==========");
+            System.out.println("Totlal space (всего места): "
+                    + totalSpace / 1024 / 1024);
+            System.out.println("Free space (свободное место): "
+                    + freeSpace / 1024 / 1024);
+            System.out.println("Usable space (доступно для использования): "
+                    + usableSpace / 1024 / 1024);
+
+        }
+    }
+
+}
